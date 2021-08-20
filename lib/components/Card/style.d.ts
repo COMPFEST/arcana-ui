b7013f26733db3c8482d5d78b9c6b2c8165a9f7d
Mario <marioserano55@gmail.com>
2021-08-20 15:49:22 +0700

    Added card

---
+++ lib/components/Card/style.d.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+declare const StyledCard: import("@emotion/styled").StyledComponent<{
+    theme?: import("@emotion/react").Theme | undefined;
+    as?: React.ElementType<any> | undefined;
+}, React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>, {}>;
+export default StyledCard;
+//# sourceMappingURL=style.d.ts.map
