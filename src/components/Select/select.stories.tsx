e23c6e0b2cceaf544d516bb616fd3fae4d4f10e8
Steven WH <stevenwh15@gmail.com>
2021-08-26 15:48:09 +0700

    select input feature

---
+++ src/components/Select/select.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+import Select from './select';
+
+export default {
+    title: 'Components/Select',
+    component: Select,
+} as Meta;
+
+const DefaultSelectTemplate: Story = (args) => <Select {...args} />;
+export const Default = DefaultSelectTemplate.bind({});
+
+Default.args = {
+    label: 'Profesi',
+    options: [
+        { value: 'mahasiswa', text: 'mahasiswa' },
+        { value: 'siswa', text: 'siswa' },
+    ],
+};
